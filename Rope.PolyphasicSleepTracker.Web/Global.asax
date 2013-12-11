290877a06f5f4dbb13e471b98428d2c89b1f70ed
Tobias Waggoner <tobias.waggoner@gmail.com>
2013-12-11 21:20:03 +0100

    2013-12-11, tw - Version 0.0.1 Initial version

---
+++ Rope.PolyphasicSleepTracker.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rope.PolyphasicSleepTracker.Web.MvcApplication" Language="C#" %>
---
+++ Rope.PolyphasicSleepTracker.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rope.PolyphasicSleepTracker.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rope.PolyphasicSleepTracker.Web")]
+[assembly: AssemblyCopyright("Copyright © Tobias Waggoner 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32d0c136-e980-442c-a837-12388edec1aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
